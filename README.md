a95bcd37bffad100c2438113ca9bff7d2bf34479
sdb <stefan@ellefant.be>
2010-10-12 23:41:31 +0200

    basic liquibase-update action

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ The liquibase-sbt-plugin is not (yet) available in a public repository, so you h
           val liquiBase = "com.github.sdb" % "liquibase-sbt-plugin" % "0.0.1"
         }
 
-2. Mixin the LiquiBasePlugin trait in your project file
+2. Mixin the LiquiBasePlugin trait in your project file, e.g. `project/build/TestProject.scala`
 
         import sbt._
         import com.github.sdb.sbt.liquibase.LiquiBasePlugin
@@ -29,3 +29,24 @@ The liquibase-sbt-plugin is not (yet) available in a public repository, so you h
         class TestProject(info: ProjectInfo) extends DefaultProject(info) with LiquiBasePlugin {
           ...
         }
+
+3. Configure
+
+        class TestProject(info: ProjectInfo) extends DefaultProject(info) with LiquiBasePlugin {
+
+          // declare the required database driver as a runtime dependency
+          val h2 = "com.h2database" % "h2" % "1.2.143" % "runtime"
+
+          // provide the parameters for running liquibase commands
+          def changeLogFile = "config" / "db-changelog.xml"
+          def driver = "org.h2.Driver"
+          def url = "jdbc:h2:mem:"
+        }
+
+#Usage#
+
+The following actions are available:
+
+* `liquibase-update`
+
+  Applies un-run changes to the database.
--- src/main/scala/liquibase.scala
+++ src/main/scala/liquibase.scala
@@ -1,5 +1,76 @@
 package com.github.sdb.sbt.liquibase
 
 import _root_.sbt._
+import Configurations.Runtime
 
-trait LiquiBasePlugin extends Project with ClasspathProject
+import _root_.liquibase.integration.commandline.CommandLineUtils
+import _root_.liquibase.resource.FileSystemResourceAccessor
+import _root_.liquibase.Liquibase
+import _root_.liquibase.exception._
+
+
+trait LiquiBasePlugin extends Project with ClasspathProject {
+
+  def changeLogFile: Path
+  def url: String
+  def driver: String
+
+  lazy val liquibaseUpdate = liquibaseUpdateAction
+  def liquibaseUpdateAction = task {
+    new LiquibaseAction with Cleanup {
+      def action { liquibase update "" }
+    }.run
+    None
+  } describedAs  "Applies un-run changes to the database."
+
+
+  trait LiquibaseAction {
+
+    lazy val database = CommandLineUtils.createDatabaseObject(
+      ClasspathUtilities.toLoader(fullClasspath(Runtime)),
+      url,
+      null,
+      null,
+      driver,
+      null,
+      null)
+
+    lazy val liquibase = new Liquibase(
+      changeLogFile.absolutePath,
+      new FileSystemResourceAccessor,
+      database)
+
+    def action
+    def run = exec({ () => action})
+    def exec(f: () => Unit) = f()
+  }
+
+  trait Cleanup extends LiquibaseAction {
+
+    override def exec(f: () => Unit) {
+      try {
+        f()
+      } finally {
+        cleanup
+      }
+    }
+
+    def cleanup {
+      if (liquibase != null)
+        try {
+          liquibase.forceReleaseLocks
+        } catch {
+          case e: LiquibaseException => log trace e
+        }
+      if (database != null)
+        try {
+          database.rollback
+          database.close
+        } catch {
+          case e: DatabaseException => log trace e
+        }
+    }
+  }
+
+}
+
