c30ecb1cd5d5d1f5c44395dac95997beae9a6d94
sdb <stefan@ellefant.be>
2010-10-15 21:42:56 +0200

    redirect logging from liquibase to SBT logger

--- src/main/scala/liquibase.scala
+++ src/main/scala/liquibase.scala
@@ -6,8 +6,10 @@ import Configurations.Runtime
 import _root_.liquibase.integration.commandline.CommandLineUtils
 import _root_.liquibase.resource.FileSystemResourceAccessor
 import _root_.liquibase.Liquibase
+import _root_.liquibase.servicelocator.ServiceLocator
 import _root_.liquibase.database.Database
 import _root_.liquibase.exception._
+import _root_.liquibase.logging.LogFactory
 
 
 trait LiquibasePlugin extends Project with ClasspathProject {
@@ -21,6 +23,10 @@ trait LiquibasePlugin extends Project with ClasspathProject {
   
   def contexts: String = null
   def defaultSchemaName: String = null
+  
+
+  // pass the project logger to receive logging from Liquibase
+  SBTLogger.logger = Some(log)
 
 
   def database = CommandLineUtils.createDatabaseObject(
@@ -120,5 +126,51 @@ trait LiquibasePlugin extends Project with ClasspathProject {
     }
   }
 
+}
+
+
+import _root_.liquibase.logging.core.AbstractLogger
+
+object SBTLogger {
+
+  // make sure that our custom Liquibase logger can be discovered by the
+  // service locator
+  ServiceLocator.getInstance.addPackageToScan("com.github.sdb.sbt.liquibase")
+
+  var logger: Option[Logger] = None
+
+  def log(l: String, m: String, e: Throwable) {
+    logger match {
+      case Some(log) => {
+        l match {
+          case "debug" => log.debug(m)
+          case "info"  => log.info(m)
+          case "warn"  => log.warn(m)
+          case "error" => log.error(m)
+        }
+        if (e != null) log.trace(e)
+      }
+      case None => {}
+    }
+  }
+}
+
+/**
+ * Custom Liquibase logger to redirect logging to the project logger.
+ */
+class SBTLogger extends AbstractLogger {
+  import SBTLogger._
+  def getPriority = 10
+  def setLogLevel(l: String, f: String) {} // ignored 
+  def setName(n: String) {} // ignored 
+
+  def debug(m: String, e: Throwable) { log("debug", m, e) }
+  def debug(m: String) { log("debug", m, null) }
+  def info(m: String, e: Throwable) { log("info", m, e) }
+  def info(m: String) { log("info", m, null) }
+  def warning(m: String, e: Throwable) { log("warn", m, e) }
+  def warning(m: String) { log("warn", m, null) }
+  def severe(m: String, e: Throwable) { log("error", m, e) }
+  def severe(m: String) { log("error", m, null) }
 }
 
